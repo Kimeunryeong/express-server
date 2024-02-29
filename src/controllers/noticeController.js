bcf160ff8942ac28e5400d156aa949f24ee53ef6
Kimeunryeong <ker26772@gmail.com>
2024-02-29 10:44:23 +0900

    20240229

--- src/controllers/noticeController.js
+++ src/controllers/noticeController.js
@@ -11,31 +11,48 @@ export const noticeList = async (req, res) => {
 
 export const noticeWrite = async (req, res) => {
   try {
-    console.log(req.body);
     const { title, description, writer } = req.body;
     const data = await Notice.create({
       title,
       description,
-      createdAt: Date.now(),
       writer,
+      createdAt: Date.now(),
     });
-    return res.send({ result: true, data: data }); //data:(data) 생략가능
-  } catch (error) {
+    return res.send({ result: true, data });
+  } catch (err) {
     console.log(err);
     return res.send({ result: false });
   }
 };
 
 export const noticeDetail = async (req, res) => {
+  // req.params.id
   const {
     params: { id },
   } = req;
   try {
     const data = await Notice.findById(id);
-    return res.send({ name: "detail", data:data });
+    return res.send({ name: "detail", data: data });
   } catch (error) {
     console.log(error);
   }
 };
-export const noticeEdit = (req, res) => res.send({ name: "edit" });
+
+export const noticeEdit = async (req, res) => {
+  const {
+    body: { title, description, writer },
+    params: { id },
+  } = req;
+
+  try {
+    const data = await Notice.findByIdAndUpdate(id, {
+      title,
+      description,
+      writer,
+    });
+    res.send({ result: true, data });
+  } catch (error) {
+    console.log(error);
+  }
+};
 export const noticeDelete = (req, res) => res.send({ name: "delete" });
